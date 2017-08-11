8953290b01be6e83525eb0287b8b860426fc735b
sriskels <sriskels@gmail.com>
2017-08-11 09:47:10 +0200

    Update main.groovy

--- test/src/main.groovy
+++ test/src/main.groovy
@@ -2,6 +2,5 @@ import pl.daffit.dautils.api.*
 chat = new DaChat("&9Test>")
 
 workspace.command("hello", "", "", []) { sender, args ->
-    (sender as Player).give(new DaItem("STONE", 1).withName("&7Hello world!").build())
-    chat.send(sender, "{PREFIX} &aHello world! :D")
+    chat.send(sender, "{PREFIX} &aHello world!")
 }
